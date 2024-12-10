1f57348deabf41ff2683ab9bdba8075b57f936fa
Emiliano Zani <142833557+emizanipro@users.noreply.github.com>
2024-12-10 10:41:42 -0300

    Add files via upload

--- js/mensaje-whatsapp.js
+++ js/mensaje-whatsapp.js
@@ -1,33 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.contact-form'); // Seleccionamos el formulario
-    const submitButton = document.getElementById('submit-button'); // Botón de envío
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.contact-form');
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-    form.addEventListener('submit', function(e) {
-        e.preventDefault(); // Evitamos que se envíe el formulario de manera tradicional
+        const name = document.querySelector('input[name="cf-name"]').value.trim();
+        const email = document.querySelector('input[name="cf-email"]').value.trim();
+        const message = document.querySelector('textarea[name="cf-message"]').value.trim();
 
-        // Obtenemos los valores de los campos del formulario
-        const name = document.querySelector('input[name="cf-name"]').value;
-        const email = document.querySelector('input[name="cf-email"]').value;
-        const message = document.querySelector('textarea[name="cf-message"]').value;
-
-        // Validar que los campos no estén vacíos
         if (!name || !email || !message) {
             alert("Por favor, completa todos los campos.");
             return;
         }
 
-        // Preparamos el mensaje para enviar por WhatsApp
         const whatsappMessage = encodeURIComponent(
             `*Presupuesto Rápido*\n\nNombre: ${name}\nEmail: ${email}\nMensaje: ${message}`
         );
 
-        // Número de teléfono de WhatsApp
         const whatsappNumber = '542615612714';
-
-        // URL de WhatsApp con el mensaje codificado
         const whatsappURL = `https://wa.me/${whatsappNumber}?text=${whatsappMessage}`;
-
-        // Usamos window.open para abrir el enlace de WhatsApp en una nueva ventana
-        window.open(whatsappURL, '_blank'); // Esto debería abrir WhatsApp Web o la app en el móvil
+        window.open(whatsappURL, '_blank');
     });
 });
